064eacb91f94e86e633fb1c2bc888652b815269b
m0s01ma <mayank.sinha@walmart.com>
2022-04-09 19:54:50 +0530

    model classes

--- src/main/java/model/Loan.java
+++ src/main/java/model/Loan.java
@@ -1,5 +1,33 @@
 package model;
 
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+@Builder
 public class Loan {
 
+    private BigDecimal principal;
+    private BigDecimal interestAmount;
+    private BigDecimal amount;
+    private Integer emiMonths;
+    private BigDecimal emiAmount;
+    private List<Payment> payments;
+
+    public Loan(@NonNull final BigDecimal principal, @NonNull final BigDecimal interestAmount, @NonNull final BigDecimal amount,
+                @NonNull final Integer emiMonths, @NonNull final BigDecimal emiAmount, @NonNull List<Payment> payments) {
+        this.principal = principal;
+        this.interestAmount = interestAmount;
+        this.amount = amount;
+        this.emiMonths = emiMonths;
+        this.emiAmount = emiAmount;
+        this.payments = payments;
+    }
+
+    public void setPayments(@NonNull List<Payment> payments) {
+        this.payments = payments;
+    }
 }
--- src/main/java/model/Payment.java
+++ src/main/java/model/Payment.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.math.BigDecimal;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+@Builder
 public class Payment {
 
+    private BigDecimal amountPaid;
+    private Integer balanceEmi;
+
+    public Payment(@NonNull final BigDecimal amountPaid, @NonNull final Integer balanceEmi) {
+        this.amountPaid = amountPaid;
+        this.balanceEmi = balanceEmi;
+    }
 }
